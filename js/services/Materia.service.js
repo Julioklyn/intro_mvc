d2e2b05378ee9b2f4ce5b42557f9686b9acbdfd1
Julioklyn <julio.carlos@aluno.ifsp.edu.br>
2025-04-11 14:22:18 -0300

    Ajustando Edicao

--- js/services/Materia.service.js
+++ js/services/Materia.service.js
@@ -1,37 +1,42 @@
 class MateriaService {
-    constructor() {
-        this.materias = [];
-        this.updateListMateriasFromLocalStorage();
-    }
+	constructor() {
+		this.materias = []
+		this.updateListMateriasFromLocalStorage()
+	}
 
-    add(materia) {
-        if (!materia instanceof MateriaModel) {
-            throw new Error('A matéria deve ser uma instância de MateriaModel');
-        }
-        this.materias.push(materia)
-        this.updateLocalStorage()
-    }
+	add(materia) {
+		if (!materia instanceof MateriaModel) {
+			throw new Error('A materia deve ser uma instância de MateriaModel')
+		}
+		this.materias.push(materia)
+		this.updateLocalStorage()
+	}
 
-    update(materia) {
-        return materia
-    }
-    
-    searchById(id) {
-        return this.materias.find(materia => materia._id === id)
-    }
+	update(materia) {
+		return materia
+	}
 
-    getAll() {
-        return this.materias;
-    }
+	searchById(id) {
+		return this.materias.find(materia => materia._id === id)
+	}
 
-    updateListMateriasFromLocalStorage() {
-        let local = localStorage.getItem('materias')
-        let materias = []
-        if (local) {
-            materias = JSON.parse(local)
-        }
-        materias.forEach(materia => {
-            this.add(new MateriaModel(materia))
-        })
-    }
+	getAll() {
+		return this.materias
+	}
+
+	updateLocalStorage() {
+		let materias = JSON.stringify(this.materias)
+		localStorage.setItem('materias', materias)
+	}
+
+	updateListMateriasFromLocalStorage() {
+		let local = localStorage.getItem('materias')
+		let materias = []
+		if (local) {
+			materias = JSON.parse(local)
+		}
+		materias.forEach(materia => {
+			this.add(new MateriaModel(materia))
+		})
+	}
 }
